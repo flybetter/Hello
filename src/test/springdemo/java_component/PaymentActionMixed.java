135b0ff7a68647c4f4fad8d3c2db0102f12355f6
flybetter <flybetter@163.com>
2019-04-17 14:43:12 +0800

    prepare for the exam for public institution

--- src/test/springdemo/java_component/PaymentActionMixed.java
+++ src/test/springdemo/java_component/PaymentActionMixed.java
@@ -1,12 +1,15 @@
 package java_component;
 
 import java_component.IOrderDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentActionMixed {
 
+    private static final Logger logger = LoggerFactory.getLogger(PaymentActionMixed.class);
 
     private IOrderDao orderDao;
 
@@ -15,7 +18,7 @@ public class PaymentActionMixed {
         this.orderDao = orderDao;
     }
 
-    public void addOrder(String  orderType) {
+    public void addOrder(String orderType) {
         this.orderDao.addOrder(orderType);
     }
 }
